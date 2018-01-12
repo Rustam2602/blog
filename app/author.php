a4f10a9ed7d009da448e346e8935d3c0344ac1ca
Rustam Affandi <rustamaffandi335@gmail.com>
2018-01-12 21:31:53 +0700

    UAS

--- app/author.php
+++ app/author.php
@@ -4,9 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Author extends Model
 {
     protected $fillable = [
-    	"name"
+    "name"
     ];
 }
